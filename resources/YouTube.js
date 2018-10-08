64ba445b7b1c1540e755a64b64f8510a0fd654e7
Unknown <Aidan.S.Edwards@gmail.com>
2018-10-08 07:56:22 -0500

    Better RYPtWS connection

---
+++ resources/YouTube.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "https://cdn.rawgit.com/Zren/ResizeYoutubePlayerToWindowSize/035b192e/153699.user.js";
+var head = document.getElementsByTagName('head')[0];
+if(head) head.appendChild(script);
