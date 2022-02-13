e198386897de5c422d49e56c40c5ee51a8a429fa
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2022-02-12 23:02:48 -0600

    Default YouTube to 2x Speed

--- resources/YouTube.js
+++ resources/YouTube.js
@@ -1,5 +1,26 @@
+var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = "https://cdn.jsdelivr.net/gh/Zren/ResizeYoutubePlayerToWindowSize/153699.user.js";
-var head = document.getElementsByTagName('head')[0];
-if(head) head.appendChild(script);
+if (head) head.appendChild(script);
+script = document.createElement('script');
+script.type = 'text/javascript';
+script.text = `
+// ==UserScript==
+// @name        Youtube Playback Rate
+// @namespace   Violentmonkey Scripts
+// @match       *://youtube.com/*
+// @match       *://www.youtube.com/*
+// @grant       Any
+// @version     1.0
+// @author      Jason Miller
+// @description 12/1/2019, 8:00:00 AM
+// @run-at      document-start
+// ==/UserScript==
+
+sessionStorage.setItem("yt-player-playback-rate", JSON.stringify({
+    "data": "2",
+    "creation": Date.now(),
+}));
+`;
+if (head) head.appendChild(script);
