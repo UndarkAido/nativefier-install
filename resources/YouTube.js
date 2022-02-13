bdf7a1224a2c92037da577b8ab27d3c5a85caa0c
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2022-02-12 23:02:39 -0600

    Move from RawGit to JSDelivr

--- resources/YouTube.js
+++ resources/YouTube.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = "https://cdn.rawgit.com/Zren/ResizeYoutubePlayerToWindowSize/035b192e/153699.user.js";
+script.src = "https://cdn.jsdelivr.net/gh/Zren/ResizeYoutubePlayerToWindowSize/153699.user.js";
 var head = document.getElementsByTagName('head')[0];
 if(head) head.appendChild(script);
